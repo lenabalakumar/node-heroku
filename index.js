fbfd0f1699cdf7f4a96a1f7de4b149a711d028dd
Lena balakumar Subbarayan <lenabalakumar@gmail.com>
2022-03-24 11:51:32 +0530

    cors middleware added

--- index.js
+++ index.js
@@ -1,7 +1,10 @@
 const express = require("express");
+const cors = require("cors");
 
 const app = express();
 const PORT = process.env.PORT || 2345;
+
+app.use(cors());
 
 app.use(function (req, res, next) {
   res.header("Access-Control-Allow-Origin", "http://localhost:3000"); // update to match the domain you will make the request from
